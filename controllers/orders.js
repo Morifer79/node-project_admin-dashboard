92cb9b2efec5b8b29b41e7bd5d5b61d6c896d4df
Hennadii Laktionov <morifer13@gmail.com>
2024-05-11 12:02:36 +0200

    Update orders.js

--- controllers/orders.js
+++ controllers/orders.js
@@ -2,12 +2,12 @@ import { Order } from '../models/order.js';
 import { ctrlWrapper } from '../helpers/ctrlWrapper.js';
 
 export const listOrders = async (req, res) => {
-  const { page = '1', limit = '5' } = req.query;
+  const { page = '1', limit = '5', name } = req.query;
   const limitNumber = parseInt(limit);
   const pageNumber = parseInt(page);
   const skipNumber = (pageNumber - 1) * limitNumber;
   const totalOrders = await Order.countDocuments();
-  const result = await Order.find()
+  const result = await Order.find({name})
     .skip(skipNumber)
     .limit(limitNumber);
 
