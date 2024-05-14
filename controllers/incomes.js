5c3b24167e2165ac4f741dc9804bb4846c1c8e66
Hennadii Laktionov <morifer13@gmail.com>
2024-05-14 17:33:26 +0200

    add preview image for readmy

--- controllers/incomes.js
+++ controllers/incomes.js
@@ -2,7 +2,7 @@ import { Income } from '../models/income.js';
 import { ctrlWrapper } from '../helpers/ctrlWrapper.js';
 
 export const listIncome = async (req, res) => {
-  const { page = 1, limit = 5 } = req.query;
+  const { page = '1', limit = '5' } = req.query;
   const skip = (page - 1) * limit;
   const result = await Income.find({ skip, limit });
   res.json(result);
